e0561457bba5359b38277f0b28b86645a4cffc0b
tuliorezende <tulio.rezende@outlook.com>
2026-03-03 21:35:33 -0300

    chore: refactor Program.cs for improved structure and async support

--- VideoProcessing/VideoProcessing.Worker/Program.cs
+++ VideoProcessing/VideoProcessing.Worker/Program.cs
@@ -1,55 +1,65 @@
+using Azure.Storage.Blobs;
+using MongoDB.Driver;
 using VideoProcessing.Application.UseCases;
 using VideoProcessing.Domain.Ports.In;
-using VideoProcessing.Infrastructure.Messaging.Configuration;
 using VideoProcessing.Domain.Ports.On;
-using VideoProcessing.Infrastructure.Providers;
 using VideoProcessing.Infrastructure.Messaging;
-using MongoDB.Driver;
-using Azure.Storage.Blobs;
+using VideoProcessing.Infrastructure.Messaging.Configuration;
+using VideoProcessing.Infrastructure.Providers;
 
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<VideoProcessingMessageConsumer>();
+namespace VideoProcessing.Worker;
 
-builder.Services.Configure<RabbitMqSettings>(
-    builder.Configuration.GetSection("RabbitMqSettings")
-);
-
-builder.Services.AddScoped<IProcessVideoUseCase, ProcessVideoUseCase>();
-builder.Services.AddSingleton<RabbitMqConnectionFactory>();
-builder.Services.AddScoped<VideoProcessedMessageProducer>();
-
-var userApiSection = builder.Configuration.GetSection("UserApi");
-var userApiBaseUrl = userApiSection.GetValue<string>("BaseUrl");
-var userApiKey = userApiSection.GetValue<string>("ApiKey");
-
-builder.Services.AddHttpClient<IUserPlanProvider, UserPlanProvider>(client =>
+public class Program
 {
-    if (!string.IsNullOrWhiteSpace(userApiBaseUrl))
+    static async Task Main(string[] args)
     {
-        client.BaseAddress = new Uri(userApiBaseUrl);
+        var builder = Host.CreateApplicationBuilder(args);
+
+        builder.Services.AddHostedService<VideoProcessingMessageConsumer>();
+
+        builder.Services.Configure<RabbitMqSettings>(
+            builder.Configuration.GetSection("RabbitMqSettings")
+        );
+
+        builder.Services.AddScoped<IProcessVideoUseCase, ProcessVideoUseCase>();
+        builder.Services.AddSingleton<RabbitMqConnectionFactory>();
+        builder.Services.AddScoped<VideoProcessedMessageProducer>();
+
+        var userApiSection = builder.Configuration.GetSection("UserApi");
+        var userApiBaseUrl = userApiSection.GetValue<string>("BaseUrl");
+        var userApiKey = userApiSection.GetValue<string>("ApiKey");
+
+        builder.Services.AddHttpClient<IUserPlanProvider, UserPlanProvider>(client =>
+        {
+            if (!string.IsNullOrWhiteSpace(userApiBaseUrl))
+            {
+                client.BaseAddress = new Uri(userApiBaseUrl);
+            }
+
+            if (!string.IsNullOrWhiteSpace(userApiKey))
+            {
+                client.DefaultRequestHeaders.Add("X-Api-Key", userApiKey);
+            }
+        });
+
+        var blobConnectionString = builder.Configuration.GetValue<string>("AzureBlob:ConnectionString");
+
+        var blobServiceClient = new BlobServiceClient(blobConnectionString);
+        builder.Services.AddSingleton(blobServiceClient);
+
+        builder.Services.AddScoped<IVideoDownloader, BlobVideoDownloader>();
+        builder.Services.AddScoped<IFrameExtractor, FfmpegFrameExtractor>();
+
+        builder.Services.AddScoped<IZipService, ZipService>();
+        builder.Services.AddScoped<IFileStorage, BlobFileStorage>();
+
+        // MongoDB client and processing repository
+        var mongoConnectionString = builder.Configuration.GetValue<string>("MongoDb:ConnectionString");
+        builder.Services.AddSingleton<IMongoClient>(sp => new MongoClient(mongoConnectionString));
+        builder.Services.AddScoped<IProcessingRepository, MongoProcessingRepository>();
+
+        var host = builder.Build();
+
+        await host.RunAsync();
     }
-
-    if (!string.IsNullOrWhiteSpace(userApiKey))
-    {
-        client.DefaultRequestHeaders.Add("X-Api-Key", userApiKey);
-    }
-});
-
-var blobConnectionString = builder.Configuration.GetValue<string>("AzureBlob:ConnectionString");
-
-var blobServiceClient = new BlobServiceClient(blobConnectionString);
-builder.Services.AddSingleton(blobServiceClient);
-
-builder.Services.AddScoped<IVideoDownloader, BlobVideoDownloader>();
-builder.Services.AddScoped<IFrameExtractor, FfmpegFrameExtractor>();
-
-builder.Services.AddScoped<IZipService, ZipService>();
-builder.Services.AddScoped<IFileStorage, BlobFileStorage>();
-
-// MongoDB client and processing repository
-var mongoConnectionString = builder.Configuration.GetValue<string>("MongoDb:ConnectionString");
-builder.Services.AddSingleton<IMongoClient>(sp => new MongoClient(mongoConnectionString));
-builder.Services.AddScoped<IProcessingRepository, MongoProcessingRepository>();
-
-var host = builder.Build();
-host.Run();
+}
